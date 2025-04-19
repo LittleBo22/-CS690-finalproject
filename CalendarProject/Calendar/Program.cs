9c66e1b78bb1c91bf1390d2d88847641b3017d1b
LittleBo22 <144484652+LittleBo22@users.noreply.github.com>
2025-04-19 16:50:37 +0000

    draft 2 of program

--- CalendarProject/Calendar/Program.cs
+++ CalendarProject/Calendar/Program.cs
@@ -1,50 +1,82 @@
 ﻿namespace CalendarProject;
 
+using System.IO;
+
 class Program
 {
+    private static string userID;
+    private static string filePath;
     static void Main(string[] args)
     {
-        Console.Write("This is the main page to log into the calendar. ");
-        Console.Write("Apartment Manager please enter admin + the first two initials of first and last name. ");
-        Console.Write("Residents please enter the first two initials of first and last name + apartment number. ");
-        string user = Console.ReadLine();
+        getuserID();
+        //Load the calendar with a method?
 
-        //display calendar
+        //show the calendar options to the user and get their choice
+        Console.WriteLine("Calendar options: ");
+        Console.WriteLine("1. Add Item");
+        Console.WriteLine("2. Edit Item");
+        Console.WriteLine("3. Delete Item");
+        Console.WriteLine("4. View Calendar");
+        Console.WriteLine("5. Exit Calendar");
+
+        Console.Write("What is your choice? ");
+        string choice = Console.ReadLine();
+        
+        switch(choice){
+            case "1":
+            break;
+            case "2":
+            break;
+            case "3":
+            break;
+            case "4":
+            break;
+            case "5":
+            break;
+            default:
+                Console.Write("That is not a valid entry.");
+                break;
+        }
+    //Method for getting the userID and creating a file for that user
+    static void getuserID(){
+        Console.WriteLine("Please enter your user name using the first two initials of first and last name + apartment number. ");
+        userID = Console.ReadLine();
+        filePath = $"calendar_{userID}.txt";
+    }   
+
+        /*display calendar
 
         Console.Write("Would you like to add, edit, delete or exit from the calendar? ");
-        string action;
+        string action = Console.ReadLine();
 
-        do {
-            if(action == "add");
+        if(action == "add");{
             Console.Write("Select the month: ");
             Console.Write("Select the day: ");
             Console.Write("Enter a title: ");
             Console.Write("Enter a description: ");
+        }    
             
             //saves changes to calendar
             //ask what the user would like to do
 
-            else if (action == "edit");
+        else if (action == "edit");{
             Console.Write("Select the month: ");
             Console.Write("Select the day: ");
             Console.Write("Select the item: ");
             Console.Write("Enter new information: ");
+        }
 
             //save changes to the calendar
             //ask what the user would like to do
             
-
-            else if(action == "delete");
+        else if(action == "delete");{
             Console.Write("Select the month: ");
             Console.Write("Select the day: ");
             Console.Write("Select the item: ");
-
+        }
             //save changes to the calendar to remove the item
             //ask the user what they would like to do
-            Console.Write("Would you like to add, edit, delete or exit from the calendar? ");
-            action = Console.ReadLine();
-
-        } while(action != "exit");  
-
+        Console.Write("Would you like to add, edit, delete or exit from the calendar? ");
+        action = Console.ReadLine();*/
     }
 }
